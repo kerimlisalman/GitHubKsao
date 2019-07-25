865c110517c812f9fc0e1d6c1bd30acd203019b8
Salman <kerimli.salman@gmail.com>
2019-07-25 18:51:15 +0400

    Create Ders2.py

---
+++ Ders2.py
@@ -0,0 +1,11 @@
+print('Как вас зовут?')
+name = input()  # считываем строку и кладём её в переменную name
+print('Здравствуйте, ' + name + '!')
+# 
+print ("birinci reqemi daxil edin ")
+a=input()
+print ("ikinci reqemi daxil edin ")
+b=input()
+c=a+b
+print (a+" + "+b+" = "+c)
+
